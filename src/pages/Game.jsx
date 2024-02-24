9d16e7b07eacc88ca6e9e9a171e2fbdb811d661a
Acalvera <calveraalejandro@gmail.com>
2024-02-24 14:04:45 +0100

    modificaciones a la pantalla de juego

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import '../styles/Game.css'
-
+import SideBar from "../components/SideBar";
 function Game () {
 
   const InfoPlayers = ({ nombreJugador, eloJugador, colorFicha, tiempoRestante, fichasComidas}) => {
@@ -27,30 +27,53 @@ function Game () {
     );
   };
 
+  const InfoGameMode = ({ GameMode }) => {
+    return (
+      <div className="game-info">
+        {GameMode} {/* Modo de juego */}
+      </div>
+    );
+  }
+
   return (
-    <div className="game-screen">
-      <div className="game">
-        <div>
-          {/* Jugador 1 */}
-          <InfoPlayers 
-          nombreJugador='Jugador 1' 
-          eloJugador='200' 
-          colorFicha='Negras' 
-          tiempoRestante='10 mins' 
-          fichasComidas='0' />
+    <div className="game-background">
+      <div className="game-menu"> {/* Temporal */}
+        <div className="game-menu-sidebar">
+          <SideBar />
         </div>
-        {/* Tablero */}
-        <div className="tablero"> {/*Introducir cuando este el tablero hecho */} 
-          Tablero
-        </div>
+      </div>
+      <div className="game-screen">
         <div>
-          {/* Jugador 2 */}
-          <InfoPlayers 
-            nombreJugador='Jugador 2'
-            eloJugador='200'  
-            colorFicha='Blancas'
+          <InfoGameMode  GameMode="Rapid" /> {/* Cambiar en funcion del modo de juego  */}
+        </div>
+        <div className="game">
+          <div>
+            {/* Jugador 1 */}
+            <InfoPlayers 
+            nombreJugador='Jugador 1' 
+            eloJugador='200' 
+            colorFicha='Negras' 
             tiempoRestante='10 mins' 
             fichasComidas='0' />
+          </div>
+          {/* Tablero */}
+          <div className="tablero"> {/*Introducir cuando este el tablero hecho */} 
+            Tablero
+          </div>
+          <div>
+            {/* Jugador 2 */}
+            <InfoPlayers 
+              nombreJugador='Jugador 2'
+              eloJugador='200'  
+              colorFicha='Blancas'
+              tiempoRestante='10 mins' 
+              fichasComidas='0' />
+          </div>
+        </div>
+      </div>
+      <div className="game-chat-box">
+        <div className="game-chat">
+          CHAT
         </div>
       </div>
     </div>
