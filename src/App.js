e4d0a7cf92df034edcf60e29fc1831a11c703057
Acalvera <calveraalejandro@gmail.com>
2024-02-24 11:37:32 +0100

    empezada pantalla de juego

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx"
 import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
+import Game from './pages/Game.jsx'
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/game' element={<Game />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Game.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import '../styles/Game.css'
+
+function Game () {
+
+  const InfoPlayers = ({ nombreJugador, eloJugador, colorFicha, tiempoRestante, fichasComidas}) => {
+    return (
+      <div>
+        <div className="game-box">
+          <div className="game-box-players">
+            <div className="game-box-players-name"> {/* Nombre del jugador y su elo */}
+              {nombreJugador} ({eloJugador})
+            </div>
+            <div className="game-box-players-token"> {/* Color de la ficha del jugador */}
+              {colorFicha}
+            </div>
+          </div>
+          <div className="game-box-time"> {/* Tiempo restante del jugador */}
+            {tiempoRestante}
+          </div>
+          <div className="game-box-tokenEaten"> {/* Cantidad de fichas comidas por el jugador */}
+            Fichas comidas: 
+            {fichasComidas}
+          </div> 
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="game-screen">
+      <div className="game">
+        <div>
+          {/* Jugador 1 */}
+          <InfoPlayers 
+          nombreJugador='Jugador 1' 
+          eloJugador='200' 
+          colorFicha='Negras' 
+          tiempoRestante='10 mins' 
+          fichasComidas='0' />
+        </div>
+        {/* Tablero */}
+        <div className="tablero"> {/*Introducir cuando este el tablero hecho */} 
+          Tablero
+        </div>
+        <div>
+          {/* Jugador 2 */}
+          <InfoPlayers 
+            nombreJugador='Jugador 2'
+            eloJugador='200'  
+            colorFicha='Blancas'
+            tiempoRestante='10 mins' 
+            fichasComidas='0' />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
