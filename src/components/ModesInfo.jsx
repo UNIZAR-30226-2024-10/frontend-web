bfac791a389990641f42d156aff257ab94c91f20
Acalvera <calveraalejandro@gmail.com>
2024-02-25 12:10:19 +0100

    cambios al home (3)

--- src/components/ModesInfo.jsx
+++ src/components/ModesInfo.jsx
@@ -10,13 +10,12 @@ function ModesInfo() {
 	const RapidInfo = "Rapid";
 	const BulletText = "Esta variante del juego se caracteriza por partidas extremadamente rápidas, \
 	en las que cada jugador cuenta con un tiempo muy limitado para realizar sus movimientos."
-	const BlitzText ="En el ajedrez relámpago, cada jugador tiene un tiempo máximo para realizar todos \
-	sus movimientos. Por lo general, este tiempo varía entre 1 y 10 minutos por partida. Esto hace que \
-	cada movimiento deba ser rápido y preciso, ya que no hay mucho margen para pensar demasiado.";
-	const RapidText = "En el ajedrez rápido, los jugadores tienen un tiempo limitado para realizar sus movimientos.\
+	const BlitzText ="En el ajedrez Blitz, cada jugador tiene un tiempo máximo para realizar todos \
+	sus movimientos. Esto hace que cada movimiento deba ser rápido y preciso, ya que no hay mucho \
+  margen para pensar demasiado.";
+	const RapidText = "En el ajedrez Rapid, los jugadores tienen un tiempo limitado para realizar sus movimientos.\
 	A diferencia de las partidas clásicas, donde se cuenta con varias horas para pensar y planificar cada jugada, \
-	en el ajedrez rápido el reloj es un factor determinante. \El tiempo máximo que se suele utilizar en el ajedrez \
-	rápido es de 15 minutos por jugador,";
+	en el ajedrez rápido el reloj es un factor determinante.";
 	const QuienesSomos = "KSJ Games es una empresa creada por jóvenes estudiantes de ingeniería informática con una pasión \
 	compartida por los videojuegos y la innovación tecnológica."
 
