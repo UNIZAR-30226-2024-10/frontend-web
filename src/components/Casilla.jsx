1addbb8662a852de8a96ea24305824b176965395
epepemi <842574@unizar.es>
2024-03-19 16:46:26 +0100

    Refactor Casilla component and remove unused images

--- src/components/Casilla.jsx
+++ src/components/Casilla.jsx
@@ -1,7 +1,22 @@
 import { useEffect, useState } from 'react';
-import { hover } from '@testing-library/user-event/dist/hover';
-import './../styles/Casilla.css'
+import '../styles/Casilla.css'
+
 const Casilla = (args) => {
+
+    const BLANCO = '#ADF597'
+    const NEGRO = '#2E960F'  
+
+    const mFila = args.rowIndex
+    const mCol = args.colIndex
+
+    //PARA MOSTRAR TABLERO CON NEGRAS ABAJO CAMBIAR LOS #COLORES
+    const colorCasilla = ((mFila+mCol) %2 === 0)? BLANCO : NEGRO
+
+    const imagen = {
+        height: '90%',
+        width: '90%',
+    }
+
     function char2Src(char) {
         switch (char) {
             case 'p':
@@ -34,58 +49,21 @@ const Casilla = (args) => {
         }   
     }
 
-    const mFila = args.rowIndex
-    const mCol = args.colIndex
-    //todas las casillas son un <div> q comparten este estilo:
-    // const estiloBase = {
-    //     display: 'flex',
-    //     alignItems: 'flex-end', /* Center vertically */
-    //     justifyContent: 'center', /* Center horizontally */
-    //     height: '100px',
-    //     width: '100px',
-    //     border: 'none',
-    //     transition: 'background-color 0.3s', // Agregamos una transición suave
-    // }
-    const imagen = {
-        height: '90%',
-        width: '90%',
-    }
-
-    const BLANCO = '#ADF597'
-    const NEGRO = '#2E960F'
     const [hovered, setHovered] = useState(false);
 
-    const handleMouseEnter = () => {
-        setHovered(true);
-    };
+    const handleMouseIn = () => {setHovered(true)}
+    const handleMouseOut = () => {setHovered(false)}
 
-    const handleMouseLeave = () => {
-        setHovered(false);
-
-    };
-
-    //PARA MOSTRAR TABLERO CON NEGRAS ABAJO CAMBIAR LOS #COLORES
-    // Dado un indice de fila y columna, determina el color de la casilla
-    const colorCasilla = (i,j) => {
-        return ((i+j) %2 === 0)? BLANCO : NEGRO
-    }
-
-    //determina el estilo de una casilla dado su indice de fila y columna
-    // const estiloCasilla = (i,j) => {
-    //     const tipoCasilla = {...estiloBase}
-    //     tipoCasilla.backgroundColor = colorCasilla(i,j)
-    //     return tipoCasilla
-    // }
-
-
+    
+    /**
+     * Maneja el evento de clic para la casilla.
+     */
     const handleClick = () => {
         console.log( args.tablero[mFila][mCol]);
         // console.log("soy "+mFila + " col "+ mCol );
         
         //Si soy una casilla con una pieza seleccionable y me seleccionan cambio piezaSel
-        if (args.tablero[mFila][mCol] !== ''
-            && '['+mFila+'-'+mCol+']' in args.movsPosibles 
-            && ((args.tablero[mFila][mCol]===args.tablero[mFila][mCol].toUpperCase() && args.turno===0) || (args.turno ===1 && args.tablero[mFila][mCol]!=args.tablero[mFila][mCol].toUpperCase()))) {
+        if (args.tablero[mFila][mCol] !== '' && '['+mFila+'-'+mCol+']' in args.movsPosibles) {
             console.log("piezaSel: ", mFila, ",", mCol)
             args.setPiezaSel({fila: mFila, col: mCol})
 
@@ -106,17 +84,17 @@ const Casilla = (args) => {
 
     
     return (
-        // <button onClick={handleClick} style={estiloCasilla(mFila, mCol)}onMouseEnter={handleMouseEnter} 
-        //     onMouseLeave={handleMouseLeave}>
+        // <button onClick={handleClick} style={estiloCasilla(mFila, mCol)}onMouseEnter={handleMouseIn} 
+        //     onMouseLeave={handleMouseOut}>
         //     {/* <img style={imagen} src={char2Src(args.tablero[fila][col])} alt="Descripción de la pieza" /> */}
         //     {char2Src(args.tablero[mFila][mCol])}
         // </button >
         <button 
             onClick={handleClick} 
-            onMouseEnter={handleMouseEnter} 
-            onMouseLeave={handleMouseLeave}
+            onMouseEnter={handleMouseIn} 
+            onMouseLeave={handleMouseOut}
             className="casilla-base" // Aplicar la clase CSS para los estilos base
-            style={{ backgroundColor: hovered ? '#CCCCCC' : colorCasilla(mFila, mCol) }} // Aplicar dinámicamente el color de la casilla
+            style={{ backgroundColor: hovered ? '#D3FFDE' : colorCasilla }} // Aplicar dinámicamente el color de la casilla
         >       {char2Src(args.tablero[mFila][mCol])}
         </button>
      );
