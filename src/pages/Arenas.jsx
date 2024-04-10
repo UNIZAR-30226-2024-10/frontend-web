664187b2ec5069c5004b6ae7899ae1c950ab90fb
Acalvera <calveraalejandro@gmail.com>
2024-04-10 21:02:48 +0200

    cambios en la página de arenas

--- src/pages/Arenas.jsx
+++ src/pages/Arenas.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import '../styles/Arenas.css'
 import SideBar from "../components/SideBar";
 import MenuIcon from '@mui/icons-material/Menu';
-
+import Tablero from '../components/Tablero';
 
 function Arenas() {
   const [showSidebar, setShowSidebar] = useState(false);
+  const arenasRef = useRef(null);
+
+  const arenas = ['Madera', 'Mármol', 'Oro', 'Esmeralda', 'Diamante'];
+
+  /* arenasRef.current.scrollTop = arenasRef.current.scrollHeight; */
 
   return (
     <div className="background-arenas">
@@ -25,7 +30,16 @@ function Arenas() {
         </button>
         <h1 className="pageTitle">ARENAS DE JUEGO</h1>
         <div className="arenas-container center">
-          
+          <ul className="arenasList" ref={arenasRef}>
+            {/* Listado de las arenas */}
+            {arenas.reverse().map((arena, index) => (
+              <li key={index}>
+                <div className="arena">
+                  <Tablero />
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
