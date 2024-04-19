25c7cdf3d152de3965de00f63b468bf9b93ea76f
Acalvera <calveraalejandro@gmail.com>
2024-04-19 15:11:55 +0200

    solo se pueden seleccionar fichas/emotes en funcion del level del usuario

--- src/pages/Personalizacion.jsx
+++ src/pages/Personalizacion.jsx
@@ -42,9 +42,6 @@ function Personalizacion () {
   const chunkSizePiezas = 4; // Number of elements per chunk
   const chunkSizeEmotes = 9;
 
-  const [fichasSelected, setFichasSelected] = useState('DEFECTO');
-  const [emotesSelected, setEmotesSelected] = useState(['😁️','😂️','👍️','😲️']);
-
   // Function to split the array into chunks
   const chunkArray = (arr, size) => {
     return arr.reduce((chunks, el, i) => {
@@ -57,25 +54,48 @@ function Personalizacion () {
     }, []);
   };
 
-  const emotesPreview = ['😁️','😂️','👍️','😲️','😭️','😅️','👊️','🤩️','🤯️','😜️','🫠️','😎️','😡️','😈️','👻️'];
-
+  // Set de piezas del usuario
+  const [fichasSelected, setFichasSelected] = useState('DEFECTO');
+  // Conjunto de emoticonos del usuario
+  const [emotesSelected, setEmotesSelected] = useState(['','','','']);
+  // Nivel del usuario (recompensas desbloqueadas)
+  const userLevel = 2;
+  // Emoticonos del juego
+  const emotesPreview = [
+    { level: 1, value: '😁️'},
+    { level: 3, value: '😂️'},
+    { level: 5, value: '👍️'},
+    { level: 7, value: '😲️'},
+    { level: 9, value: '😭️'},
+    { level: 11, value: '😅️'},
+    { level: 13, value: '👊️'},
+    { level: 15, value: '🤩️'},
+    { level: 17, value: '🤯️'},
+    { level: 19, value: '😜️'},
+    { level: 21, value: '🫠️'},
+    { level: 23, value: '😎️'},
+    { level: 25, value: '😡️'},
+    { level: 27, value: '😈️'},
+    { level: 29, value: '👻️'},
+  ];
+  // Piezas del juego
   const piezasPreview = [
-    { modelo: 'DEFECTO', negras : [DefaultBK,DefaultBQ,DefaultBN,DefaultBB,DefaultBR,DefaultBP], blancas : [DefaultWK,DefaultWQ,DefaultWN,DefaultWB,DefaultWR,DefaultWP] },
-    { modelo: 'ALPHA', negras : [AlphaBK,AlphaBQ,AlphaBN,AlphaBB,AlphaBR,AlphaBP], blancas : [AlphaWK,AlphaWQ,AlphaWN,AlphaWB,AlphaWR,AlphaWP] },
-    { modelo: 'CARDINAL', negras: [CardinalBK, CardinalBQ, CardinalBN, CardinalBB, CardinalBR, CardinalBP], blancas: [CardinalWK, CardinalWQ, CardinalWN, CardinalWB, CardinalWR, CardinalWP] },
-    { modelo: 'CELTIC', negras : [CelticBK,CelticBQ,CelticBN,CelticBB,CelticBR,CelticBP], blancas : [CelticWK,CelticWQ,CelticWN,CelticWB,CelticWR,CelticWP] },
-    { modelo: 'CHESS7', negras: [Chess7BK, Chess7BQ, Chess7BN, Chess7BB, Chess7BR, Chess7BP], blancas: [Chess7WK, Chess7WQ, Chess7WN, Chess7WB, Chess7WR, Chess7WP] },
-    { modelo: 'CHESSNUT', negras : [ChessnutBK,ChessnutBQ,ChessnutBN,ChessnutBB,ChessnutBR,ChessnutBP], blancas : [ChessnutWK,ChessnutWQ,ChessnutWN,ChessnutWB,ChessnutWR,ChessnutWP] },
-    { modelo: 'COMPANION', negras: [CompanionBK, CompanionBQ, CompanionBN, CompanionBB, CompanionBR, CompanionBP], blancas: [CompanionWK, CompanionWQ, CompanionWN, CompanionWB, CompanionWR, CompanionWP] },
-    { modelo: 'FANTASY', negras : [FantasyBK,FantasyBQ,FantasyBN,FantasyBB,FantasyBR,FantasyBP], blancas : [FantasyWK,FantasyWQ,FantasyWN,FantasyWB,FantasyWR,FantasyWP] },
-    { modelo: 'FRESCA', negras: [FrescaBK, FrescaBQ, FrescaBN, FrescaBB, FrescaBR, FrescaBP], blancas: [FrescaWK, FrescaWQ, FrescaWN, FrescaWB, FrescaWR, FrescaWP] },
-    { modelo: 'GOVERNOR', negras : [GovernorBK,GovernorBQ,GovernorBN,GovernorBB,GovernorBR,GovernorBP], blancas : [GovernorWK,GovernorWQ,GovernorWN,GovernorWB,GovernorWR,GovernorWP] },
-    { modelo: 'KOSAL', negras: [KosalBK, KosalBQ, KosalBN, KosalBB, KosalBR, KosalBP], blancas: [KosalWK, KosalWQ, KosalWN, KosalWB, KosalWR, KosalWP] },
-    { modelo: 'LEIPZIG', negras : [LeipzigBK,LeipzigBQ,LeipzigBN,LeipzigBB,LeipzigBR,LeipzigBP], blancas : [LeipzigWK,LeipzigWQ,LeipzigWN,LeipzigWB,LeipzigWR,LeipzigWP] },
-    { modelo: 'MPCHESS', negras: [MpchessBK, MpchessBQ, MpchessBN, MpchessBB, MpchessBR, MpchessBP], blancas: [MpchessWK, MpchessWQ, MpchessWN, MpchessWB, MpchessWR, MpchessWP] },
-    { modelo: 'PIXEL', negras : [PixelBK,PixelBQ,PixelBN,PixelBB,PixelBR,PixelBP], blancas : [PixelWK,PixelWQ,PixelWN,PixelWB,PixelWR,PixelWP] },
-    { modelo: 'MAESTRO', negras: [MaestroBK, MaestroBQ, MaestroBN, MaestroBB, MaestroBR, MaestroBP], blancas: [MaestroWK, MaestroWQ, MaestroWN, MaestroWB, MaestroWR, MaestroWP] },
-    { modelo: 'ANARCANDY', negras : [AnarcandyBK,AnarcandyBQ,AnarcandyBN,AnarcandyBB,AnarcandyBR,AnarcandyBP], blancas : [AnarcandyWK,AnarcandyWQ,AnarcandyWN,AnarcandyWB,AnarcandyWR,AnarcandyWP] },
+    { level: 0, modelo: 'DEFECTO', negras : [DefaultBK,DefaultBQ,DefaultBN,DefaultBB,DefaultBR,DefaultBP], blancas : [DefaultWK,DefaultWQ,DefaultWN,DefaultWB,DefaultWR,DefaultWP] },
+    { level: 2, modelo: 'ALPHA', negras : [AlphaBK,AlphaBQ,AlphaBN,AlphaBB,AlphaBR,AlphaBP], blancas : [AlphaWK,AlphaWQ,AlphaWN,AlphaWB,AlphaWR,AlphaWP] },
+    { level: 4, modelo: 'CARDINAL', negras: [CardinalBK, CardinalBQ, CardinalBN, CardinalBB, CardinalBR, CardinalBP], blancas: [CardinalWK, CardinalWQ, CardinalWN, CardinalWB, CardinalWR, CardinalWP] },
+    { level: 6, modelo: 'CELTIC', negras : [CelticBK,CelticBQ,CelticBN,CelticBB,CelticBR,CelticBP], blancas : [CelticWK,CelticWQ,CelticWN,CelticWB,CelticWR,CelticWP] },
+    { level: 8, modelo: 'CHESS7', negras: [Chess7BK, Chess7BQ, Chess7BN, Chess7BB, Chess7BR, Chess7BP], blancas: [Chess7WK, Chess7WQ, Chess7WN, Chess7WB, Chess7WR, Chess7WP] },
+    { level: 10, modelo: 'CHESSNUT', negras : [ChessnutBK,ChessnutBQ,ChessnutBN,ChessnutBB,ChessnutBR,ChessnutBP], blancas : [ChessnutWK,ChessnutWQ,ChessnutWN,ChessnutWB,ChessnutWR,ChessnutWP] },
+    { level: 12, modelo: 'COMPANION', negras: [CompanionBK, CompanionBQ, CompanionBN, CompanionBB, CompanionBR, CompanionBP], blancas: [CompanionWK, CompanionWQ, CompanionWN, CompanionWB, CompanionWR, CompanionWP] },
+    { level: 14, modelo: 'FANTASY', negras : [FantasyBK,FantasyBQ,FantasyBN,FantasyBB,FantasyBR,FantasyBP], blancas : [FantasyWK,FantasyWQ,FantasyWN,FantasyWB,FantasyWR,FantasyWP] },
+    { level: 16, modelo: 'FRESCA', negras: [FrescaBK, FrescaBQ, FrescaBN, FrescaBB, FrescaBR, FrescaBP], blancas: [FrescaWK, FrescaWQ, FrescaWN, FrescaWB, FrescaWR, FrescaWP] },
+    { level: 18, modelo: 'GOVERNOR', negras : [GovernorBK,GovernorBQ,GovernorBN,GovernorBB,GovernorBR,GovernorBP], blancas : [GovernorWK,GovernorWQ,GovernorWN,GovernorWB,GovernorWR,GovernorWP] },
+    { level: 20, modelo: 'KOSAL', negras: [KosalBK, KosalBQ, KosalBN, KosalBB, KosalBR, KosalBP], blancas: [KosalWK, KosalWQ, KosalWN, KosalWB, KosalWR, KosalWP] },
+    { level: 22, modelo: 'LEIPZIG', negras : [LeipzigBK,LeipzigBQ,LeipzigBN,LeipzigBB,LeipzigBR,LeipzigBP], blancas : [LeipzigWK,LeipzigWQ,LeipzigWN,LeipzigWB,LeipzigWR,LeipzigWP] },
+    { level: 24, modelo: 'MPCHESS', negras: [MpchessBK, MpchessBQ, MpchessBN, MpchessBB, MpchessBR, MpchessBP], blancas: [MpchessWK, MpchessWQ, MpchessWN, MpchessWB, MpchessWR, MpchessWP] },
+    { level: 26, modelo: 'PIXEL', negras : [PixelBK,PixelBQ,PixelBN,PixelBB,PixelBR,PixelBP], blancas : [PixelWK,PixelWQ,PixelWN,PixelWB,PixelWR,PixelWP] },
+    { level: 28, modelo: 'MAESTRO', negras: [MaestroBK, MaestroBQ, MaestroBN, MaestroBB, MaestroBR, MaestroBP], blancas: [MaestroWK, MaestroWQ, MaestroWN, MaestroWB, MaestroWR, MaestroWP] },
+    { level: 30, modelo: 'ANARCANDY', negras : [AnarcandyBK,AnarcandyBQ,AnarcandyBN,AnarcandyBB,AnarcandyBR,AnarcandyBP], blancas : [AnarcandyWK,AnarcandyWQ,AnarcandyWN,AnarcandyWB,AnarcandyWR,AnarcandyWP] },
   ];
 
   const piezasChunks = chunkArray(piezasPreview, chunkSizePiezas);
@@ -145,8 +165,9 @@ function Personalizacion () {
               /* Listado de aspectos de piezas */
               <div className="menuDeslizanteContenido">
                 {piezasChunks[currentPagePiezas - 1]?.map((piezas, i) => (
-                  <button key={i} className={`listadoPiezas ${fichasSelected === piezas.modelo ? 'selected' : ''}`}
-                    onClick={() => setFichasSelected(piezas.modelo)}>
+                  <button key={i} className={userLevel >= piezas.level ? (`listadoPiezas ${fichasSelected === piezas.modelo ? 'selected' : ''}`)
+                  : ("listadoPiezas locked")}
+                    onClick={() => setFichasSelected(piezas.modelo)} disabled={userLevel < piezas.level}>
                     <div className="modeloPiezas">
                       {piezas.modelo}
                     </div>
@@ -167,9 +188,10 @@ function Personalizacion () {
               /* Listado de emoticonos */
               <div className="menuDeslizanteContenido">
                 {emotesChunks[currentPageEmotes - 1]?.map((emotes, i) => (
-                  <button key={i} className={`listadoEmotes ${emotesSelected.find(selected => selected == emotes) ? 'selected' : ''}`}
-                    onClick={() => addEmote(emotes)}>
-                    {emotes}
+                  <button key={i} className={userLevel >= emotes.level ? (`listadoEmotes ${emotesSelected.find(selected => selected == emotes.value) ? 'selected' : ''}`)
+                   : ("listadoEmotes locked")}
+                    onClick={() => addEmote(emotes.value)} disabled={userLevel < emotes.level}>
+                    {emotes.value}
                   </button>
                 ))}
               </div> 
